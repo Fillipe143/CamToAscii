4323360dfdba95cf79835d5cb12d50a18417d3eb
Fillipe143 <fillipedev.contato@gmail.com>
2024-05-27 17:06:10 -0300

    added smooth frame change

--- main.go
+++ main.go
@@ -21,21 +21,23 @@ func main() {
 		gocv.CvtColor(frame, &frame, gocv.ColorBGRToRGB)
 		gocv.Flip(frame, &frame, 1)
 
+		var b bytes.Buffer
 		for y := 0; y < frame.Rows(); y++ {
-			var b bytes.Buffer
 			for x := 0; x < frame.Cols(); x++ {
 				pixel := frame.GetVecbAt(y, x)
 				v := pixel[0] + pixel[1] + pixel[2]
 				b.WriteRune(getCharForBrightness(int(v)))
 
-                // Draw colors instead chars
+				// Draw colors instead chars
 				// b.WriteString(fmt.Sprintf("\x1b[48;2;%d;%d;%dm", pixel[0], pixel[1], pixel[2]))
 				// b.WriteRune(' ')
 				// b.WriteString("\x1b[0m")
 			}
 
-			fmt.Println(b.String())
+            b.WriteRune('\n')
 		}
+		fmt.Println(b.String())
+		fmt.Printf("\033[%dA\033[%dD", frame.Rows(), frame.Cols())
 	}
 }
 
